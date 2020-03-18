ab8001ace8921ad7eff050059e971d753d8e1072
xlc1016 <xlc101603@163.com>
2020-03-18 12:47:13 +0800

    添加test包

---
+++ src/test/java/com/xlc/community/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xlc.community.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
